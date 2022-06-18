640faeb0fc547bd757eaf34ba167c212b6a203d9
Kadir Akyol <102226254+kadirakyol@users.noreply.github.com>
2022-06-18 15:52:07 +0300

    Some Codes

--- Alerts/ViewController.swift
+++ Alerts/ViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var usernameText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    
+    @IBOutlet weak var passwordAgainText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signUpButton(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Error !", message: "UserName Not Found", preferredStyle: UIAlertController.Style.alert)
+        
+        self.present(alert, animated: true,  completion: nil)
+        
+    }
+    
 }
